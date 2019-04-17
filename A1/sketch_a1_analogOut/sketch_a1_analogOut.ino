f066efa0b106c13f3eec83059acfdc5d87d17807
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-04-17 10:41:02 -0700

    adjusting delays

--- A1/sketch_a1_analogOut/sketch_a1_analogOut.ino
+++ A1/sketch_a1_analogOut/sketch_a1_analogOut.ino
@@ -20,15 +20,15 @@ int b = 0;
 int g = 0;
 int rgb[3];
 
-float vibgyor[7] = {276.0, 209.0, 240.0, 120.0, 60.0, 30.0, 0.0};
-float vibgyor_s[7] = {1.0, 0.83, 1.0, 1.0, 1.0, 1.0, 1.0};
-float vibgyor_l[7] = {0.69, 0.3, 0.5, 0.25, 0.5, 0.5, 0.5};
-int colorIndex = 2;
-float hue = 276.0;
-float saturation = 1.0;
-float lightness = 0.69;
+float vibgyor[7] = {276.0, 180.0, 240.0, 120.0, 33.0, 347.0, 0.0};
+float vibgyor_s[7] = {1.0, 0.6, 1.0, 1.0, 1.0, 1.0, 1.0};
+float vibgyor_l[7] = {0.69, 0.65, 0.5, 0.25, 0.5, 0.85, 0.5};
+int colorIndex = 0;
+float hue;
+float saturation;
+float lightness;
 
-const int DELAY = 1000; // delay in ms between changing colors
+const int DELAY = 200; // delay in ms between changing colors
 
 void setup() {
   // Set the RGB pins to output
@@ -50,7 +50,7 @@ void loop() {
   photocellReading = analogRead(PHOTOCELL_PIN); 
   Serial.print("Photocell reading = ");
   Serial.println(photocellReading);     // the raw analog reading
-  delay(10);
+  delay(20);
   // LED gets brighter the darker it is at the sensor
   // that means we have to -invert- the reading from 0-1023 back to 1023-0
   photocellReading = 1023 - photocellReading;
@@ -65,11 +65,11 @@ void loop() {
   Serial.print("Pressure reading = ");
   Serial.println(newPressure);
 
+  hue = vibgyor[colorIndex];
+  saturation = vibgyor_s[colorIndex];
+  lightness = vibgyor_l[colorIndex];
   if ((newPressure - oldPressure) > 50) {
     colorIndex = (colorIndex + 1) % 7;
-    hue = vibgyor[colorIndex];
-    saturation = vibgyor_s[colorIndex];
-    lightness = vibgyor_l[colorIndex];
     Serial.print("Pressure diff ====== ");
     Serial.println(hue);
     Serial.println(lightness);
@@ -78,81 +78,45 @@ void loop() {
 
   hsl2rgb(hue/360.0, saturation, lightness * (LEDbrightness/255.0), rgb);
 
-  Serial.print("::: ");
-  Serial.print(rgb[0]);
-  Serial.print(rgb[1]);
-  Serial.println(rgb[2]);
   analogWrite(RGB_RED_PIN, 255 - rgb[0]);     // turn on the red LED
   analogWrite(RGB_GREEN_PIN, 255 - rgb[1]);  // turn off the green LED
   analogWrite(RGB_BLUE_PIN, 255 - rgb[2]);    // turn on the blue LED
+  delay(DELAY);
 }
 
 
 // http://courses.ischool.berkeley.edu/i262/f13/content/clemensmeyer/lab-3-potentiometers-hsl-rgb.html
 
 void hsl2rgb (float h, float s, float l, int rgb[]) {
-
   // Variables for conversion
-
   float q = 0;
-
   float t = 0;
-
-      
-
-  if (s == 0) {
-
-    r = g = b = l;  //achromatic
-
-  } else {
-
-    if (l < 0.5) {
-
-      q = l * (1 + s);
-
-    } else {
-
-      q = l + s - l * s;
-
-    }
-
-    float p = 2 * l - q;
-
-    
-
-    r = hue2rgb(p, q, h + 0.33) * 255;
-
-    g = hue2rgb(p, q, h) * 255;
-
-    b = hue2rgb(p, q, h - 0.33) * 255;
-
-  }
-
   
-
+  if (s == 0) {
+    r = g = b = l;  //achromatic
+  } else {
+    if (l < 0.5) {
+      q = l * (1 + s);
+    } else {
+      q = l + s - l * s;
+    }
+    float p = 2 * l - q;
+    r = hue2rgb(p, q, h + 0.33) * 255;
+    g = hue2rgb(p, q, h) * 255;
+    b = hue2rgb(p, q, h - 0.33) * 255;
+  }
   rgb[0] = r;
-
   rgb[1] = g;
-
   rgb[2] = b;
-
 }
 
  
 
 float hue2rgb (float p, float q, float t) {
-
   if(t < 0) {t += 1;}
-
   if(t > 1) {t -= 1;}
-
   if(t < 0.17) {return p + (q - p) * 6 * t;}
-
   if(t < 0.5) {return q;}
-
   if(t < 0.67) {return p + (q - p) * (0.67 - t) * 6;}
-
   return p;
- 
-
 }
