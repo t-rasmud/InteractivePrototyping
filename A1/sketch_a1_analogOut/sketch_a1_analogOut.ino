9149d0124a8fbf2a11ca8a3889769a378d95fa67
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-04-17 17:29:41 -0700

    code documented

--- A1/sketch_a1_analogOut/sketch_a1_analogOut.ino
+++ A1/sketch_a1_analogOut/sketch_a1_analogOut.ino
@@ -1,24 +1,36 @@
-const int RGB_RED_PIN = 6;
-const int RGB_GREEN_PIN  = 5;
-const int RGB_BLUE_PIN  = 3;
+/*
+ * Assignment 1 : Interactive night light
+ * Takes input from 2 sources - a photo resistive cell and a pressure sensor.
+ * Change in ambient light controls the brightness of the RGB LED.
+ * Change in pressure causes a change in hue. 
+ * The LED switches among the 7 colors of a rainbow (mostly).
+ * Every time all the 7 colors have been displayed, prints
+ * "Rainbow" on the serial monitor which is used to increment
+ * a rainbow count on a webpage.
+ * 
+ * Fragments of code taken from https://github.com/jonfroehlich/CSE599Sp2019/blob/master/
+ */
 
-const int PHOTOCELL_PIN = A0;    // the cell and 10K pulldown are connected to A0
-int photocellReading;     // the analog reading from the sensor divider
-int LEDbrightness;        // 
-int LEDbrightness_hsl;
-int oldPressure = 0;
+const int RGB_RED_PIN = 6;        // Output pin for Red.
+const int RGB_GREEN_PIN  = 5;     // Output pin for Green.
+const int RGB_BLUE_PIN  = 3;      // Output pin for Blue.
+
+const int PHOTOCELL_PIN = A0;     // the cell and 10K pulldown are connected to A0
+int photocellReading;             // the analog reading from the sensor divider (photocell)
+int LEDbrightness;                // input values of the photocell mapped to 0 - 255.
+
+const int PRESSURE_PIN = A5;      // Input pin for pressure sensor (DIY lo-fi)
+int pressureCellReading;          // input values of the pressure sensor mapped to 0 - 255.
+int oldPressure = 0;              // these two values are used to maintain the pressure difference.
 int newPressure = 0;
 
-const int PRESSURE_PIN = A5;
-int pressureCellReading;
-double temp = 0.0;
-
-// RGB Values
-int r = 0;        // Variables to store the RGB color values
+// Variables to store the RGB color values mapped from HSL colorspace.
+int r = 0;        
 int b = 0;
 int g = 0;
 int rgb[3];
 
+// The seven Rainbow (VIBGYOR) colors.
 float vibgyor[7] = {276.0, 180.0, 240.0, 120.0, 33.0, 347.0, 0.0};
 float vibgyor_s[7] = {1.0, 0.6, 1.0, 1.0, 1.0, 1.0, 1.0};
 float vibgyor_l[7] = {0.69, 0.65, 0.5, 0.25, 0.5, 0.85, 0.5};
@@ -27,7 +39,13 @@ float hue;
 float saturation;
 float lightness;
 
-const int DELAY = 30; // delay in ms between changing colors
+const int DELAY = 10; // delay in ms between changing colors
+
+const int MIN_PHOTOCELL_VAL = 100; // Photocell reading in dark
+const int MAX_PHOTOCELL_VAL = 990; // Photocell reading in ambient light
+
+const int MIN_PRESSURE_VAL = 370; // Minimum value read from pressure sensor
+const int MAX_PRESSURE_VAL = 630; // Maximum value read from pressure sensor
 
 void setup() {
   // Set the RGB pins to output
@@ -35,57 +53,65 @@ void setup() {
   pinMode(RGB_GREEN_PIN, OUTPUT);
   pinMode(RGB_BLUE_PIN, OUTPUT);
 
-  // Turn on Serial so we can verify expected colors via Serial Monitor
+  // Turn on Serial so we can verify input values via Serial Monitor.
   Serial.begin(9600); 
 }
 
 void loop() {
-
-  // Because the RGB LED we purchased for the class is a 'common anode'
-  // RGB, the way we turn on each light is counter to our intuition
-  // We set a pin to LOW to turn on the corresponding LED to its maximum brightness
-  // and HIGH to turn it off (the opposite of what you might think)
-
+  // Read input photocell value.
   photocellReading = analogRead(PHOTOCELL_PIN); 
   Serial.print("Photocell reading = ");
   Serial.println(photocellReading);     // the raw analog reading
-  delay(20);
+  delay(DELAY);
+
+  // Read input pressure sensor value.
+  pressureCellReading = analogRead(PRESSURE_PIN);
+  Serial.print("Pressure reading = ");
+  Serial.println(pressureCellReading);
+
+  // Calculate the brightness to be displayed based on photocell value.
   // LED gets brighter the darker it is at the sensor
   // that means we have to -invert- the reading from 0-1023 back to 1023-0
   photocellReading = 1023 - photocellReading;
   //now we have to map 0-1023 to 0-255 since thats the range analogWrite uses
-  LEDbrightness = map(photocellReading, 100, 990, 0, 255);
+  LEDbrightness = map(photocellReading, MIN_PHOTOCELL_VAL, MAX_PHOTOCELL_VAL, 0, 255);
   LEDbrightness = constrain(LEDbrightness, 0, 255);
-  // http://eduardofv.com/2011/01/15/arduino-rgb-led-hsv-color-wheel/
 
-  pressureCellReading = analogRead(PRESSURE_PIN);
-  newPressure = map(pressureCellReading, 370, 630, 0, 255);
+  // Calculate the difference in pressure between the previous reading and the current one.
+  // Switch LED hue if the pressure has increased by at least 50.
+  newPressure = map(pressureCellReading, MIN_PRESSURE_VAL, MIN_PRESSURE_VAL, 0, 255);
   newPressure = constrain(newPressure, 0, 255);
-  Serial.print("Pressure reading = ");
-  Serial.println(pressureCellReading);
-
   hue = vibgyor[colorIndex];
   saturation = vibgyor_s[colorIndex];
   lightness = vibgyor_l[colorIndex];
   if ((newPressure - oldPressure) > 50) {
     colorIndex = (colorIndex + 1) % 7;
     if(colorIndex == 0) {
+      // All 7 colors have been displayed.
       Serial.println("Rainbow");
     }
   }
   oldPressure = newPressure;
 
+  // Convert HSL to RGB. 
   hsl2rgb(hue/360.0, saturation, lightness * (LEDbrightness/255.0), rgb);
 
+  // Output to RGB LED.
   analogWrite(RGB_RED_PIN, 255 - rgb[0]);     // turn on the red LED
   analogWrite(RGB_GREEN_PIN, 255 - rgb[1]);  // turn off the green LED
   analogWrite(RGB_BLUE_PIN, 255 - rgb[2]);    // turn on the blue LED
-  delay(10);
+  delay(DELAY);
 }
 
 
-// http://courses.ischool.berkeley.edu/i262/f13/content/clemensmeyer/lab-3-potentiometers-hsl-rgb.html
-
+/*
+ * Function to convert HSL to RGB colorpace. 
+ * Code taken from: http://courses.ischool.berkeley.edu/i262/f13/content/clemensmeyer/lab-3-potentiometers-hsl-rgb.html 
+ * param h: hue ranges from 0 - 1.
+ * param s: saturation ranges from 0 - 1.
+ * param l: lightness ranges from 0 - 1.
+ * param rgb: array to store the output rgb values.
+ */
 void hsl2rgb (float h, float s, float l, int rgb[]) {
   // Variables for conversion
   float q = 0;
@@ -109,8 +135,9 @@ void hsl2rgb (float h, float s, float l, int rgb[]) {
   rgb[2] = b;
 }
 
- 
-
+/*
+ * Also taken from http://courses.ischool.berkeley.edu/i262/f13/content/clemensmeyer/lab-3-potentiometers-hsl-rgb.html
+ */
 float hue2rgb (float p, float q, float t) {
   if(t < 0) {t += 1;}
   if(t > 1) {t -= 1;}
